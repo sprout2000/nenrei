d26a2b0d5fb5ab84ab647b8b6563ef6df1badd78
sprout2000 <wassabie64@gmail.com>
2021-01-21 15:16:23 +0900

    refactor: use esbuild instead of webpack

---
+++ scripts/build.ts
@@ -0,0 +1,11 @@
+import { build } from 'esbuild';
+
+build({
+  define: { 'process.env.NODE_ENV': process.env.NODE_ENV as string },
+  entryPoints: ['src/main.tsx'],
+  outdir: 'public',
+  bundle: true,
+  minify: true,
+  target: 'es2015',
+  platform: 'browser',
+}).catch(() => process.exit(1));
