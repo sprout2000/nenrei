19fa6d78a43302577016e7facb02d8e0732d2f25
sprout2000 <wassabie64@gmail.com>
2021-02-18 14:57:40 +0900

    chore: remove unused loaders & plugins

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,8 +4,6 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CopyWebpackPlugin from 'copy-webpack-plugin';
 import TerserWebpackPlugin from 'terser-webpack-plugin';
 import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import CssMinimizerWebpackPlugin from 'css-minimizer-webpack-plugin';
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -22,7 +20,6 @@ const config: Configuration = {
     path: path.resolve(__dirname, 'public'),
     publicPath: '',
     filename: '[name].js',
-    assetModuleFilename: 'images/[name][ext]',
   },
   module: {
     rules: [
@@ -31,67 +28,37 @@ const config: Configuration = {
         exclude: /node_modules/,
         loader: 'ts-loader',
       },
-      {
-        test: /\.css$/,
-        use: [
-          isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: isDev,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(ico|gif|jpe?g|png|svg|ttf|otf|eot|woff?2?)$/,
-        type: 'asset/resource',
-      },
     ],
   },
-  plugins: isDev
-    ? [
-        new CopyWebpackPlugin({ patterns: [{ from: 'assets', to: '.' }] }),
-        new HtmlWebpackPlugin({
-          template: './src/index.html',
-          favicon: './src/favicon.ico',
-          inject: 'body',
-          scriptLoading: 'blocking',
-          minify: false,
-        }),
-      ]
-    : [
-        new MiniCssExtractPlugin(),
-        new CopyWebpackPlugin({ patterns: [{ from: 'assets', to: '.' }] }),
-        new HtmlWebpackPlugin({
-          template: './src/index.html',
-          favicon: './src/favicon.ico',
-          inject: 'body',
-          scriptLoading: 'defer',
-          minify: true,
-        }),
-        new WorkboxWebpackPlugin.GenerateSW({
-          swDest: 'service-worker.js',
-          skipWaiting: true,
-          clientsClaim: true,
-        }),
-      ],
-  cache: isDev
-    ? {
-        type: 'filesystem',
-        cacheDirectory: path.resolve(__dirname, '.cache'),
-      }
-    : false,
+  plugins: [
+    new CopyWebpackPlugin({ patterns: [{ from: 'assets', to: '.' }] }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/favicon.ico',
+      inject: 'body',
+      scriptLoading: 'defer',
+      minify: !isDev,
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: 'service-worker.js',
+      skipWaiting: true,
+      clientsClaim: true,
+    }),
+  ],
+  cache: {
+    type: 'filesystem',
+    cacheDirectory: path.resolve(__dirname, '.cache'),
+  },
   optimization: {
-    minimize: isDev ? false : true,
-    minimizer: [new TerserWebpackPlugin(), new CssMinimizerWebpackPlugin()],
+    minimize: true,
+    minimizer: [new TerserWebpackPlugin()],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
     port: 7890,
     open: true,
   },
-  devtool: isDev ? 'source-map' : false,
+  devtool: isDev ? 'inline-source-map' : false,
   stats: 'errors-only',
   performance: { hints: false },
 };
