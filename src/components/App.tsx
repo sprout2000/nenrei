c47c3b97bf83b40b35734ca312f2e8c3e2bd4671
sprout2000 <wassabie64@gmail.com>
2021-02-18 14:49:18 +0900

    refactor: do not bundle icon-288

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -26,8 +26,6 @@ import Snack from './Snack';
 import SideBar from './SideBar';
 import TitleBar from './TitleBar';
 
-import Icon from './icon-288.png';
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const typeguardStorage = (arg: any): arg is Storage => {
   return (
@@ -254,7 +252,12 @@ const App: React.FC = () => {
         <div className={classes.content}>
           <div className={classes.offset} />
           <div className={classes.icon}>
-            <img src={Icon} width={96} height={96} alt="年齢計算" />
+            <img
+              src="./icons/icon-288.png"
+              width={96}
+              height={96}
+              alt="年齢計算"
+            />
           </div>
           <Snack snackOpen={snackOpen} onClose={onSnackClose} />
           <Card className={classes.card}>
