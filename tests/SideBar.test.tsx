a4d65878da8b5e71c177358d876c5dca0a4eb2ce
sprout2000 <wassabie64@gmail.com>
2021-03-10 08:45:31 +0900

    chore: update SideBar.test.tsx

--- tests/SideBar.test.tsx
+++ tests/SideBar.test.tsx
@@ -7,23 +7,35 @@ import SideBar from '../src/components/SideBar';
 
 describe('SideBar component', () => {
   test('render SideBar component', () => {
+    window.open = jest.fn();
+    const onToggle = jest.fn();
+    const onQROpen = jest.fn();
+    const onSnackOpen = jest.fn();
+
     render(
       <SideBar
         drawerOpen={true}
-        onQROpen={() => console.log('onQROpen(): void')}
-        onSnackOpen={() => console.log('onSnackOpen(): void')}
-        toggleDrawer={() => console.log('toggleDrawer(): void')}
+        onQROpen={onQROpen}
+        onSnackOpen={onSnackOpen}
+        toggleDrawer={onToggle}
       />
     );
 
-    expect(screen.getByText(/年齢計算 v/)).toBeInTheDocument();
-
-    expect(screen.getByAltText(/Logo/)).toHaveAttribute(
+    expect(screen.getByRole('img')).toHaveAttribute(
       'src',
       './icons/icon-288.png'
     );
 
-    userEvent.click(screen.getByText(/このアプリを共有/));
-    userEvent.click(screen.getByText(/ライセンスの表示/));
+    userEvent.click(screen.getByRole('button', { name: 'このアプリを共有' }));
+    expect(onQROpen).toBeCalledTimes(1);
+    expect(onToggle).toBeCalledTimes(1);
+
+    userEvent.click(screen.getByRole('button', { name: 'レポジトリ' }));
+    expect(window.open).toBeCalledTimes(1);
+    expect(onToggle).toBeCalledTimes(2);
+
+    userEvent.click(screen.getByRole('button', { name: 'ライセンスの表示' }));
+    expect(onSnackOpen).toBeCalledTimes(1);
+    expect(onToggle).toBeCalledTimes(3);
   });
 });
