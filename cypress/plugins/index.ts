2c215f3b55d8a9151587e55bfa3f2cca6da679d1
sprout2000 <wassabie64@gmail.com>
2021-09-19 13:51:25 +0900

    chore: Updated dependencies.

--- cypress/plugins/index.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-
-const config = (on: string, config: string) => {};
-export default config;
