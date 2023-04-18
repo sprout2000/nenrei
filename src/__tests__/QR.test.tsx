cd43cf220acdbf7f18985f8debf0a0f36f1833cf
sprout2000 <wassabie64@gmail.com>
2023-04-18 12:36:34 +0900

    test: Remove QR.test.tsx

--- src/__tests__/QR.test.tsx
+++
@@ -1,7 +0,0 @@
-import "jest-canvas-mock";
-import { render } from "@testing-library/react";
-import { QR } from "../QR";
-
-test("render QR component", () => {
-  render(<QR onClose={() => jest.fn()} qrOpen={true} />);
-});
